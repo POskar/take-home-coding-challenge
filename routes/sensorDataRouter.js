7bd7cb0a568784cf80f1138f98afeba2829542f8
Oskar Pieniak <45405600+POskar@users.noreply.github.com>
2023-04-26 12:22:44 +0200

    added handling of resolution

--- routes/sensorDataRouter.js
+++ routes/sensorDataRouter.js
@@ -48,10 +48,87 @@ router.get('/', async (req, res) => {
     query.room = room
   }
 
-  let data
+  let data;
   try {
-    data = await SensorData.find(query)
+    if (resolution === 'hourly') {
+      const hourlyData = await SensorData.aggregate([
+        { $match: query },
+        { $group: {
+            _id: {
+              year: { $year: '$datetime' },
+              month: { $month: '$datetime' },
+              day: { $dayOfMonth: '$datetime' },
+              hour: { $hour: '$datetime' },
+              room: '$room',
+              measurement: '$measurement'
+            },
+            avgValue: { $avg: '$value' }
+          }
+        },
+        { $sort: { _id: 1 } }
+      ])
 
+      data = hourlyData.map(hour => {
+        return {
+          value: hour.avgValue,
+          datetime: new Date(Date.UTC(hour._id.year, hour._id.month - 1, hour._id.day, hour._id.hour)),
+          room: hour._id.room,
+          measurement: hour._id.measurement
+        }
+      })
+    } else if (resolution === 'daily') {
+      const dailyData = await SensorData.aggregate([
+        { $match: query },
+        { $group: {
+            _id: {
+              year: { $year: '$datetime' },
+              month: { $month: '$datetime' },
+              day: { $dayOfMonth: '$datetime' },
+              room: '$room',
+              measurement: '$measurement'
+            },
+            avgValue: { $avg: '$value' }
+          }
+        },
+        { $sort: { _id: 1 } }
+      ])
+      
+      data = dailyData.map(day => {
+        return {
+          value: day.avgValue,
+          datetime: new Date(Date.UTC(day._id.year, day._id.month - 1, day._id.day)),
+          room: day._id.room,
+          measurement: day._id.measurement
+        }
+      })
+    } else if (resolution === 'weekly') {
+      const weeklyData = await SensorData.aggregate([
+        { $match: query },
+        { $group: {
+            _id: {
+              year: { $year: '$datetime' },
+              week: { $isoWeek: "$datetime" },
+              room: '$room',
+              measurement: '$measurement'
+            },
+            avgValue: { $avg: '$value' }
+          }
+        },
+        { $sort: { _id: 1 } }
+      ])
+    
+      data = weeklyData.map(week => {
+        return {
+          value: week.avgValue,
+          datetime: new Date(Date.UTC(week._id.year, 0, (week._id.week - 1) * 7 + 1)),
+          room: week._id.room,
+          measurement: week._id.measurement
+        }
+      })
+    } else {
+      data = await SensorData.find(query)
+    }
+    
     if (data.length == 0) {
       return res.status(404).json({ message: 'Cannot find any sensor read that matches this criteria.' })
     }
